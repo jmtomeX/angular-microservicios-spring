f2d6b8ab2fb4e8d1688380180007f85d492238e6
jmtomeX <jmtome00@gmail.com>
2021-02-01 08:15:54 +0100

    creados componentes principales y añadidas las rutas a los componentes principales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlumnosComponent } from './components/alumnos/alumnos.component';
+import { ExamenesComponent } from './components/examenes/examenes.component';
+import { CursosComponent } from './components/cursos/cursos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlumnosComponent,
+    ExamenesComponent,
+    CursosComponent
   ],
   imports: [
     BrowserModule,
